2871e94095f693437900b73289e860ca545bad59
chariss <chariss.chomba@equitybank.co.ke>
2021-07-27 17:56:43 +0300

    Queue implementation with stack

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,22 +8,12 @@ import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) {
-       ArrayQueue queue = new ArrayQueue(5);
-       queue.enqueue(10);
-       queue.enqueue(20);
-       queue.enqueue(30);
-        System.out.println(queue);
+        QueueWithTwoStacks queue = new QueueWithTwoStacks();
+        queue.enqueue(10);
+        queue.enqueue(20);
         System.out.println(queue.dequeue());
-        System.out.println(queue);
-        queue.enqueue(40);
-        queue.enqueue(50);
-        queue.enqueue(60);
-        System.out.println(queue.dequeue());
-        queue.enqueue(70);
-        System.out.println(queue);
         System.out.println(queue.peek());
+        System.out.println(queue.dequeue());
         System.out.println(queue.isEmpty());
-        System.out.println(queue.isFull());
-
     }
 }
